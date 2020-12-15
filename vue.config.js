b19a7ebab4a007bf29df6311197355963bc3a043
会飞的野马 <jay@huifeideyemadeMacBook-Pro.local>
2020-12-15 23:06:00 +0800

    引入antd,初始化vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+//vue.config.js
+module.exports = {
+    // 基本路径
+    publicPath: process.env.NODE_ENV === 'production' ? '' : './',
+    // 输出文件目录
+    outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
+    // eslint-loader 是否在保存的时候检查
+    lintOnSave: false
+}
